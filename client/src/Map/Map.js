f386dd51ae2a952c5ed6f5ed1cd4a2f571d72aa8
TimRomanowich <romanowt@oregonstate.edu>
2023-10-31 14:05:33 -0700

    Added point of interest icon and popup message.

--- client/src/Map/Map.js
+++ client/src/Map/Map.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import { defaultIcon } from "../icons/defaultIcon";
 
 export const Map = () => {
   const position = [44.5638, -123.2794];
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <Marker position={position} icon={defaultIcon}>
+        <Popup>
+          Popup message displays when icon is clicked. <br /> How customizable?
+        </Popup>
+      </Marker>
     </MapContainer>
   );
 };
