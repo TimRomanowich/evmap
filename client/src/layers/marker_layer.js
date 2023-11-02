59d16769f2621f662d636f7af119c7a097352451
TimRomanowich <romanowt@oregonstate.edu>
2023-11-02 08:01:28 -0700

    added event handler for onclick

--- client/src/layers/marker_layer.js
+++ client/src/layers/marker_layer.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Marker, Popup, Tooltip } from "react-leaflet";
+import { Marker, Popup, Tooltip, useMap } from "react-leaflet";
 import { chargerIcon } from "../icons/chargerIcon";
 
 export const MarkerLayer = ({ data }) => {
-
+  const leafletMap = useMap();
     return data.features.map((feature) => {
       const { coordinates } = feature.geometry;
       const { "Station Name":stationName, ZIP } = feature.properties;
@@ -12,6 +12,9 @@ export const MarkerLayer = ({ data }) => {
           key={String(coordinates)}
           position={[coordinates[1], coordinates[0]]}
           icon={chargerIcon}
+          eventHandlers={{
+            click: (e) => leafletMap.panTo(e.latlng),
+          }}
         >
         <Tooltip>
             Station: {stationName} <br />
